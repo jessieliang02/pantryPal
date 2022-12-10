544bf4f5f9c8416c7f9586f6a44b2107b18bbde8
Zhiqing Liang <zliang305@gatech.edu>
2022-12-10 03:32:46 -0800

    Initial Commit

---
+++ pantryPal/pantryPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  pantryPalApp.swift
+//  pantryPal
+//
+//  Created by Jessie Liang on 2022-12-10.
+//
+
+import SwiftUI
+
+@main
+struct pantryPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
